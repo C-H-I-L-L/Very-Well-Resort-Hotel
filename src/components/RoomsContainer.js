fa213f72f807b5f9e0761d6e6084bce14aaa7691
C-H-I-L-L <fartandgohome@gmail.com>
2020-01-03 16:40:24 -0700

    on the rooms page - before changing guests drop-down values from string

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -19,37 +19,29 @@
 
 // export default withRoomConsumer(RoomContainer);
 
-
-
-
-
 // Below is how to to the same thing as above, without higher-order components
 
-import React from 'react'
-import RoomsFilter from './RoomsFilter'
-import RoomsList from './RoomsList'
-import {RoomConsumer} from '../context'
-import Loading from './Loading'
+import React from 'react';
+import RoomsFilter from './RoomsFilter';
+import RoomsList from './RoomsList';
+import { RoomConsumer } from '../context';
+import Loading from './Loading';
 
 export default function RoomsContainer() {
-    return (
-        <RoomConsumer>
-            {value => {
-                    const {loading,sortedRooms,rooms} = value;
-                    if (loading) {
-                        return <Loading />
-                    }
-                    return ( 
-                        <div>
-                            <RoomsFilter rooms={rooms} />
-                            <RoomsList rooms={sortedRooms}/>
-                        </div>
-                    );
-                }
-            }
-
-
-        </RoomConsumer>
-        
-    )
+  return (
+    <RoomConsumer>
+      {value => {
+        const { loading, sortedRooms, rooms } = value;
+        if (loading) {
+          return <Loading />;
+        }
+        return (
+          <div>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+          </div>
+        );
+      }}
+    </RoomConsumer>
+  );
 }
