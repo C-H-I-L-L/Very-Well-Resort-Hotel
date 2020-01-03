17f3b9b5363efc8daa1d35266573ddfa7e38c75f
C-H-I-L-L <fartandgohome@gmail.com>
2020-01-02 23:53:09 -0700

    Added rooms filter functionality

--- src/context.js
+++ src/context.js
@@ -59,14 +59,38 @@ class RoomProvider extends Component {
   };
 
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = event.type === 'checkbox' ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(type, name, value);
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
 
-  filterRoom = () => {
-    console.log('hello');
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+
+    // if type is set to anything BUT all, then filter it
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms
+    });
   };
   render() {
     return (
